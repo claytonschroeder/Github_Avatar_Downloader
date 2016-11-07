bf74eece05c0fe835b3241abf5f58f80fa735156
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2016-11-07 20:26:16 +0000

    created requestURL in getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,17 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!')
 
+var GITHUB_USER = "claytonschroeder";
+var GITHUB_TOKEN = "08357d858988d04f39d7ce95381a38d8c2eeaf47";
+
   function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
